76cab082d1fb08941d7d5d8d7b2eec456ce60fc5
Athena <athena@parthenon.app>
2024-01-06 16:53:20 -0800

    fix(keyboard): Updated spacing and font for buttons

--- src/components/keyboard/index.tsx
+++ src/components/keyboard/index.tsx
@@ -6,11 +6,13 @@ import styles from './index.module.scss';
 const Keyboard = ({ onKeyUp, keyColors, keys }: KeyboardProps) => {
   const KeyTile = ({ id }: KeyTileProps) => {
     const isBackspace = id === 'Backspace';
-    let styleNames = keyColors[id]
-      ? styles[keyColors[id]]
-      : isBackspace
-      ? styles.backspace
-      : undefined;
+    const isEnter = id === 'Enter';
+
+    let styleNames;
+
+    if (keyColors[id]) styleNames = styles[keyColors[id]];
+    else if (isBackspace) styleNames = styles.backspace;
+    else if (isEnter) styleNames = styles.enter;
 
     return (
       <button className={styleNames} onClick={() => onKeyUp(id)}>
