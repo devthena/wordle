8d95fff8a070f6dffa8d302788a3933f2c940e2a
Athena <athena@parthenon.app>
2024-01-10 20:57:12 -0800

    fix(landing): Updated styles for the play section

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -8,41 +8,43 @@ Chart.register(BarElement, CategoryScale, LinearScale);
 
 const Landing = ({ setStatus }: LandingProps) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.playContainer}>
-        <h1>Wordle</h1>
-        <button onClick={() => setStatus(GameStatus.SoloStart)}>
-          PLAY SOLO
-        </button>
-        <h2>Co-op Rooms</h2>
-        <button>Create Room</button>
-      </div>
-      <div className={styles.statsContainer}>
-        <div className={styles.stats}>
-          <h3>Solo Stats</h3>
-          <div>
-            <p>Win Percentage: 97%</p>
-            <p>Max Streak: 3</p>
-            <p>Current Streak: 2</p>
-            <p>Total Times Played: 11</p>
-          </div>
+    <div className={styles.content}>
+      <h1>Wordle</h1>
+      <div className={styles.container}>
+        <div className={styles.playContainer}>
+          <h2>Solo</h2>
+          <button onClick={() => setStatus(GameStatus.SoloStart)}>PLAY</button>
+          <h2>Co-op</h2>
+          <button>Create Room</button>
+          <button>Join Room</button>
         </div>
-        <Bar
-          className={styles.chart}
-          options={{ indexAxis: 'y' }}
-          data={{
-            labels: ['1', '2', '3', '4', '5', '6'],
-            datasets: [
-              {
-                label: 'Guess Distribution',
-                data: [1, 7, 27, 75, 53, 3],
-                backgroundColor: 'rgba(106, 170, 100, 0.5)',
-                borderColor: 'rgb(106, 170, 100)',
-                borderWidth: 1,
-              },
-            ],
-          }}
-        />
+        <div className={styles.statsContainer}>
+          <div className={styles.stats}>
+            <h3>Solo Stats</h3>
+            <div>
+              <p>Win Percentage: 97%</p>
+              <p>Max Streak: 3</p>
+              <p>Current Streak: 2</p>
+              <p>Total Times Played: 11</p>
+            </div>
+          </div>
+          <Bar
+            className={styles.chart}
+            options={{ indexAxis: 'y' }}
+            data={{
+              labels: ['1', '2', '3', '4', '5', '6'],
+              datasets: [
+                {
+                  label: 'Guess Distribution',
+                  data: [1, 7, 27, 75, 53, 3],
+                  backgroundColor: 'rgba(106, 170, 100, 0.5)',
+                  borderColor: 'rgb(106, 170, 100)',
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
+        </div>
       </div>
     </div>
   );
