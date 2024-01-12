1577ad4525f231220e33fd4c3689673b13cd1e49
Athena <athena@parthenon.app>
2024-01-12 02:17:32 -0800

    feat(header): Added a stats icon button for mobile landing

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,4 @@
+import { StatsIcon } from '../../constants/icons';
 import { HeaderProps } from '../../constants/types';
 import LogoutButton from '../logout-button';
 import styles from './index.module.scss';
@@ -5,11 +6,18 @@ import styles from './index.module.scss';
 const Header = ({ avatar, username }: HeaderProps) => {
   return (
     <header className={styles.container}>
-      {username && <span className={styles.greeting}>Hi, {username}!</span>}
-      {avatar && (
-        <img alt="User Avatar" className={styles.avatar} src={avatar} />
-      )}
-      <LogoutButton />
+      <div className={styles.buttonContainer}>
+        <button className={styles.stats}>
+          <StatsIcon />
+        </button>
+      </div>
+      <div className={styles.userContainer}>
+        {username && <span className={styles.greeting}>Hi, {username}!</span>}
+        {avatar && (
+          <img alt="User Avatar" className={styles.avatar} src={avatar} />
+        )}
+        <LogoutButton />
+      </div>
     </header>
   );
 };
