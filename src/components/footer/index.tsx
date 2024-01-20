88084f073394facbe379640f6cf83aa9f8040f72
Athena <athena@parthenon.app>
2024-01-19 17:18:00 -0800

    refactor(components): Moved the Logout button for mobile and updated styles for Loading component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,18 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import { FooterProps } from '../../constants/types';
-import LogoutButton from '../logout-button';
 import styles from './index.module.scss';
 
 const Footer = ({ version }: FooterProps) => {
-  const { isAuthenticated } = useAuth0();
-
   return (
     <footer className={styles.container}>
-      {isAuthenticated && (
-        <div className={styles.logout}>
-          <LogoutButton />
-        </div>
-      )}
       <p>Made with ♡ by Athena | Build v{version}</p>
     </footer>
   );
--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,5 +1,7 @@
+import styles from './index.module.scss';
+
 const Loading = () => {
-  return <p>Loading...</p>;
+  return <p className={styles.loading}>Loading...</p>;
 };
 
 export default Loading;
